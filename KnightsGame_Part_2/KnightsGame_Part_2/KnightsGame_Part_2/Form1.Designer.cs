44b8b59ceba9a9e9def392b0f2735098c3e7f636
mzakotin <70580871+mzakotin@users.noreply.github.com>
2022-08-04 10:36:10 +0200

    Add files via upload

---
+++ KnightsGame_Part_2/KnightsGame_Part_2/KnightsGame_Part_2/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace KnightsGame_Part_2
+{
+    partial class fm_KnightsGame_2
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_knight5 = new System.Windows.Forms.Button();
+            this.bt_knight4 = new System.Windows.Forms.Button();
+            this.bt_knight3 = new System.Windows.Forms.Button();
+            this.bt_knight2 = new System.Windows.Forms.Button();
+            this.bt_knight1 = new System.Windows.Forms.Button();
+            this.tB_diceNumber = new System.Windows.Forms.TextBox();
+            this.L_Knight_5 = new System.Windows.Forms.Label();
+            this.L_Knight_4 = new System.Windows.Forms.Label();
+            this.L_Knight_3 = new System.Windows.Forms.Label();
+            this.L_Knight_2 = new System.Windows.Forms.Label();
+            this.L_Knight_1 = new System.Windows.Forms.Label();
+            this.L_DiceNumber = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt_knight5
+            // 
+            this.bt_knight5.Location = new System.Drawing.Point(67, 139);
+            this.bt_knight5.Name = "bt_knight5";
+            this.bt_knight5.Size = new System.Drawing.Size(51, 30);
+            this.bt_knight5.TabIndex = 23;
+            this.bt_knight5.Text = "100";
+            this.bt_knight5.UseVisualStyleBackColor = true;
+            this.bt_knight5.Click += new System.EventHandler(this.bt_knight5_Click);
+            // 
+            // bt_knight4
+            // 
+            this.bt_knight4.Location = new System.Drawing.Point(122, 211);
+            this.bt_knight4.Name = "bt_knight4";
+            this.bt_knight4.Size = new System.Drawing.Size(51, 30);
+            this.bt_knight4.TabIndex = 22;
+            this.bt_knight4.Text = "100";
+            this.bt_knight4.UseVisualStyleBackColor = true;
+            this.bt_knight4.Click += new System.EventHandler(this.bt_knight4_Click);
+            // 
+            // bt_knight3
+            // 
+            this.bt_knight3.Location = new System.Drawing.Point(221, 190);
+            this.bt_knight3.Name = "bt_knight3";
+            this.bt_knight3.Size = new System.Drawing.Size(51, 30);
+            this.bt_knight3.TabIndex = 21;
+            this.bt_knight3.Text = "100";
+            this.bt_knight3.UseVisualStyleBackColor = true;
+            this.bt_knight3.Click += new System.EventHandler(this.bt_knight3_Click);
+            // 
+            // bt_knight2
+            // 
+            this.bt_knight2.Location = new System.Drawing.Point(203, 123);
+            this.bt_knight2.Name = "bt_knight2";
+            this.bt_knight2.Size = new System.Drawing.Size(51, 30);
+            this.bt_knight2.TabIndex = 20;
+            this.bt_knight2.Text = "100";
+            this.bt_knight2.UseVisualStyleBackColor = true;
+            this.bt_knight2.Click += new System.EventHandler(this.bt_knight2_Click);
+            // 
+            // bt_knight1
+            // 
+            this.bt_knight1.Location = new System.Drawing.Point(140, 69);
+            this.bt_knight1.Name = "bt_knight1";
+            this.bt_knight1.Size = new System.Drawing.Size(51, 30);
+            this.bt_knight1.TabIndex = 19;
+            this.bt_knight1.Text = "100";
+            this.bt_knight1.UseVisualStyleBackColor = true;
+            this.bt_knight1.Click += new System.EventHandler(this.bt_knight1_Click);
+            // 
+            // tB_diceNumber
+            // 
+            this.tB_diceNumber.Location = new System.Drawing.Point(112, 20);
+            this.tB_diceNumber.Name = "tB_diceNumber";
+            this.tB_diceNumber.ReadOnly = true;
+            this.tB_diceNumber.Size = new System.Drawing.Size(20, 20);
+            this.tB_diceNumber.TabIndex = 18;
+            // 
+            // L_Knight_5
+            // 
+            this.L_Knight_5.AutoSize = true;
+            this.L_Knight_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.L_Knight_5.Location = new System.Drawing.Point(64, 123);
+            this.L_Knight_5.Name = "L_Knight_5";
+            this.L_Knight_5.Size = new System.Drawing.Size(54, 13);
+            this.L_Knight_5.TabIndex = 17;
+            this.L_Knight_5.Text = "Knight 5";
+            // 
+            // L_Knight_4
+            // 
+            this.L_Knight_4.AutoSize = true;
+            this.L_Knight_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.L_Knight_4.Location = new System.Drawing.Point(119, 195);
+            this.L_Knight_4.Name = "L_Knight_4";
+            this.L_Knight_4.Size = new System.Drawing.Size(54, 13);
+            this.L_Knight_4.TabIndex = 16;
+            this.L_Knight_4.Text = "Knight 4";
+            // 
+            // L_Knight_3
+            // 
+            this.L_Knight_3.AutoSize = true;
+            this.L_Knight_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.L_Knight_3.Location = new System.Drawing.Point(218, 174);
+            this.L_Knight_3.Name = "L_Knight_3";
+            this.L_Knight_3.Size = new System.Drawing.Size(54, 13);
+            this.L_Knight_3.TabIndex = 15;
+            this.L_Knight_3.Text = "Knight 3";
+            // 
+            // L_Knight_2
+            // 
+            this.L_Knight_2.AutoSize = true;
+            this.L_Knight_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.L_Knight_2.Location = new System.Drawing.Point(200, 107);
+            this.L_Knight_2.Name = "L_Knight_2";
+            this.L_Knight_2.Size = new System.Drawing.Size(54, 13);
+            this.L_Knight_2.TabIndex = 14;
+            this.L_Knight_2.Text = "Knight 2";
+            // 
+            // L_Knight_1
+            // 
+            this.L_Knight_1.AutoSize = true;
+            this.L_Knight_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.L_Knight_1.Location = new System.Drawing.Point(137, 53);
+            this.L_Knight_1.Name = "L_Knight_1";
+            this.L_Knight_1.Size = new System.Drawing.Size(54, 13);
+            this.L_Knight_1.TabIndex = 13;
+            this.L_Knight_1.Text = "Knight 1";
+            // 
+            // L_DiceNumber
+            // 
+            this.L_DiceNumber.AutoSize = true;
+            this.L_DiceNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.L_DiceNumber.Location = new System.Drawing.Point(26, 23);
+            this.L_DiceNumber.Name = "L_DiceNumber";
+            this.L_DiceNumber.Size = new System.Drawing.Size(80, 13);
+            this.L_DiceNumber.TabIndex = 12;
+            this.L_DiceNumber.Text = "dice number:";
+            // 
+            // fm_KnightsGame_2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 314);
+            this.Controls.Add(this.bt_knight5);
+            this.Controls.Add(this.bt_knight4);
+            this.Controls.Add(this.bt_knight3);
+            this.Controls.Add(this.bt_knight2);
+            this.Controls.Add(this.bt_knight1);
+            this.Controls.Add(this.tB_diceNumber);
+            this.Controls.Add(this.L_Knight_5);
+            this.Controls.Add(this.L_Knight_4);
+            this.Controls.Add(this.L_Knight_3);
+            this.Controls.Add(this.L_Knight_2);
+            this.Controls.Add(this.L_Knight_1);
+            this.Controls.Add(this.L_DiceNumber);
+            this.Name = "fm_KnightsGame_2";
+            this.Text = "Knights game - Part 2";
+            this.Load += new System.EventHandler(this.fm_KnightsGame_2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_knight5;
+        private System.Windows.Forms.Button bt_knight4;
+        private System.Windows.Forms.Button bt_knight3;
+        private System.Windows.Forms.Button bt_knight2;
+        private System.Windows.Forms.Button bt_knight1;
+        private System.Windows.Forms.TextBox tB_diceNumber;
+        private System.Windows.Forms.Label L_Knight_5;
+        private System.Windows.Forms.Label L_Knight_4;
+        private System.Windows.Forms.Label L_Knight_3;
+        private System.Windows.Forms.Label L_Knight_2;
+        private System.Windows.Forms.Label L_Knight_1;
+        private System.Windows.Forms.Label L_DiceNumber;
+    }
+}
+
